add22e4417f36dd684ae7fb901318c1c0d025767
Cheukyin Yip <cheukyinyip0077@gmail.com>
2014-07-09 16:55:58 +0800

    Add plugins in elpa/

---
+++ elpa/flymake-shell-0.8/flymake-shell-autoloads.el
@@ -0,0 +1,29 @@
+;;; flymake-shell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (flymake-shell-load) "flymake-shell" "flymake-shell.el"
+;;;;;;  (21422 10705 465886 819000))
+;;; Generated autoloads from flymake-shell.el
+
+(autoload 'flymake-shell-load "flymake-shell" "\
+Configure flymake mode to check the current buffer's shell-script syntax.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("flymake-shell-pkg.el") (21422 10705 617287
+;;;;;;  591000))
+
+;;;***
+
+(provide 'flymake-shell-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-shell-autoloads.el ends here
---
+++ elpa/idomenu-20111122.1048/idomenu-autoloads.el
@@ -0,0 +1,29 @@
+;;; idomenu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (idomenu) "idomenu" "idomenu.el" (21422 10732 593887
+;;;;;;  826000))
+;;; Generated autoloads from idomenu.el
+
+(autoload 'idomenu "idomenu" "\
+Switch to a buffer-local tag from Imenu via Ido.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("idomenu-pkg.el") (21422 10732 678008
+;;;;;;  420000))
+
+;;;***
+
+(provide 'idomenu-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idomenu-autoloads.el ends here
