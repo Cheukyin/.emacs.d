b8f46df55340c0c7407c8bce916ab8a6b1300593
Cheukyin Yip <cheukyinyip0077@gmail.com>
2014-07-11 15:45:32 +0800

    Add mouse-wheel acceleration to make mouse-scrolling more smooth

--- init-smooth-scroll.el
+++ init-smooth-scroll.el
@@ -6,7 +6,7 @@
 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-progressive-speed 3) ;; don't accelerate scrolling
 
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 
