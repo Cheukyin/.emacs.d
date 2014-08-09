4149b02718a52f5dcc02768534c02f42032087b1
Cheukyin Yip <cheukyinyip0077@gmail.com>
2014-08-09 20:58:25 +0800

    Use pyvenv.el and can change the virtualenv of jedi now

--- init-jedi.el
+++ init-jedi.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-jedi")
 (require 'jedi)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/pyvenv")
+(require 'pyvenv)
+
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)                      ; optional
 (setq jedi:complete-on-dot t)                 ; optional
